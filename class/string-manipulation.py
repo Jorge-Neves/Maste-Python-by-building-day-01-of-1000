49d90ec88dc66ccc8ec65d666aed9720e06d80bb
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-06-26 09:06:30 +0100

    Day 01 exercise 01 and 02

---
+++ class/string-manipulation.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("""Hello World!
+Hello World!
+Hello World!""")
+
+print("Hello" + "Angela")
+
+print("Hello" + " " + "Angela")
